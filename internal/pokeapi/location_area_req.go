b728f0b5d418386336d40a46483c8f0ecd92eaf6
Puran Singh <purandixit@gmail.com>
2023-04-17 13:46:31 +0530

    added command files etc

---
+++ internal/pokeapi/location_area_req.go
@@ -0,0 +1,42 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func (c *Client) ListLocationAreas() (LocationAreasResp, error) {
+	endpoint := "/location-area-test"
+	fullURL := baseURL + endpoint
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 399 {
+		return LocationAreasResp{}, fmt.Errorf("bad status code: %v", resp.StatusCode)
+	}
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	locationAreaResp := LocationAreasResp{}
+	err = json.Unmarshal(dat, &locationAreaResp)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	return locationAreaResp, nil
+}
--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Print(" >")
-	scanner.Scan()
-	text := scanner.Text()
-	fmt.Println("echoing: ", text)
+	startRepl()
 }
